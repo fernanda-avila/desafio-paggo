d3eacb733b897309e92810e3b185b8c21dcf295a
fernanda-avila <avilafernandaa@gmail.com>
2024-12-15 23:04:15 -0300

    🎨 Atualizar estilos globais e de página, ajustando cores e layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     <div className={styles.pageContainer}>
       <h1 className={styles.title}>Bem-vindo ao imgRead</h1>
       <p className={styles.description}>O imgRead é um OCR File Reader, e permite que você faça upload de imagens para extrair textos automaticamente.
-      Basta enviar um arquivo e nós cuidamos do resto.</p>
+      </p>
       <Link href="/users">
         <button className={styles.button}>Usar a Plataforma</button>
       </Link>
