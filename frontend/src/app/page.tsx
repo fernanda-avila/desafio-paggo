ab9f8df9f8882de31132cd09a56d4349d651ed69
fernanda-avila <avilafernandaa@gmail.com>
2024-12-16 13:05:36 -0300

    🎨 Atualizar título da aplicação para "ImgRead" e adicionar meta tag no cabeçalho da página inicial

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,25 @@
 
 import Link from 'next/link';
 import styles from '../app/homePage/home.module.css';
+import Head from 'next/head';
+
+
 
 const HomePage = () => {
   return (
-    <div className={styles.pageContainer}>
-      <h1 className={styles.title}>Bem-vindo ao imgRead</h1>
-      <p className={styles.description}>O imgRead é um OCR File Reader, e permite que você faça upload de imagens para extrair textos automaticamente.
-      </p>
-      <Link href="/users">
-        <button className={styles.button}>Usar a Plataforma</button>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>imgRead - Home</title>
+      </Head>
+      <div className={styles.pageContainer}>
+        <h1 className={styles.title}>Bem-vindo ao imgRead</h1>
+        <p className={styles.description}>O imgRead é um OCR File Reader, e permite que você faça upload de imagens para extrair textos automaticamente.
+        </p>
+        <Link href="/users">
+          <button className={styles.button}>Usar a Plataforma</button>
+        </Link>
+      </div>
+    </>
   );
 };
 
